b3abb86f791520bab7ac98c521b58fde2dd33600
LiubomyrTyryk123 <133780817+LiubomyrTyryk123@users.noreply.github.com>
2023-06-12 23:10:13 +0200

    lab 3 zad 1

--- zad1.py
+++ zad1.py
@@ -1,17 +1,6 @@
-import math
-a = int(input("Podaj współczynnik a: "))
-b = int(input("Podaj współczynnik b: "))
-c = int(input("Podaj współczynnik c: "))
-if a != 0:
-    D = b*b - 4 * a * c
-    if D > 0:
-        x1 = (-b + math.sqrt(D))/(2 * a)
-        x2 = (-b - math.sqrt(D))/(2 * a)
-        print(f"Rozwiązania równania: x1 = {x1}, x2 = {x2}")
-    elif D == 0:
-        x = -b/(2 * a)
-        print(f"Rozwiązanie równania: x = {x}")
+def sil(a):
+    if (a == 0):
+        return 1 
     else:
-        print("Błąd rozwiązań rzeczywistych")
-else:
-    print("Nie jest równaniem kwadratowym")
+        return sil(a - 1) * a
+print(sil(int(input("Podaj liczbę:"))))
