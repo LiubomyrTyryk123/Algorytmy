0040be0d7afe72b0c351238ea7e0958c52cb2d62
LiubomyrTyryk123 <133780817+LiubomyrTyryk123@users.noreply.github.com>
2023-06-12 23:08:55 +0200

    lab1 zad 3

---
+++ zad3.py
@@ -0,0 +1,7 @@
+liczby = [1,2,3,4,5,6,7,8,9,10]
+liczba = int(input("Podaj wartość:"))
+
+if liczba in liczby:
+    print(f"Wartość {liczba} występuje w tablicy.")
+else:
+    print(f"Wartość {liczba} nie występuje w tablicy.")
